a5218a3895262affb26786468539bb48b0910430
Davi Oliveira <107776531+Davi0115@users.noreply.github.com>
2022-07-13 00:06:07 -0300

    funcional

--- fazer-media/index.js
+++ fazer-media/index.js
@@ -1,41 +1,26 @@
-let notasArr = [0, 0, 0, 0];
-
-function mediaNotas(notasArr) {
-    var i = 0; 
-    somaNotas = 0;
-    tamanhoArr = notasArr.length; 
-    while (i < tamanhoArr) {
-        somaNotas += notasArr[i++];
-}
-    return somaNotas / tamanhoArr;
-}
-
-/* tester */
-var a = mediaNotas(notasArr);
-console.log(a);
+function adicionaNotas(){
+    let nota1 = parseFloat(document.getElementById("nota1").value)
+    let nota2 = parseFloat(document.getElementById("nota2").value)
+    let nota3 = parseFloat(document.getElementById("nota3").value)
+    let nota4 = parseFloat(document.getElementById("nota4").value)
+    let textoMedia = document.getElementById("texto-media");
 
 
+    let media = (nota1 + nota2+ nota3 + nota4)/4 ;
 
-
-
-
-/* let counter = 0;
-let counterEl = document.getElementById("count-el");
-let saveEl = document.getElementById("save-el")
-
-function increment(){
-    counter++;
-    counterEl.innerHTML = counter;
+    if(media >= 9){
+        textoMedia.innerHTML = "Aprovado:" + media + " Parabéns!";
+        document.getElementById("texto-media").style.color = "blue";
+    }
+    else if(media < 7 ){
+        textoMedia.innerHTML = "Reprovado: " + media;
+        document.getElementById("texto-media").style.color = "red";
+    } else{
+        textoMedia.innerHTML = "Aprovado: " + media;
+        document.getElementById("texto-media").style.color = "green";
+    }
 }
 
-function save(){
-    let saveCount = counter + " - "
-    saveEl.textContent += saveCount;
-    counter = 0;
-    counterEl.innerText = 0;
-}
+function limpar(){
 
-function incrementThree(){
-    counter += 3;
-    counterEl.innerHTML= counter;
-} */
+}
