be4309807a3133d5e87860375e56ebea8896148e
Davi Oliveira <107776531+Davi0115@users.noreply.github.com>
2022-07-14 18:46:24 -0300

    splice and slice

---
+++ array_study/splice-slice.js
@@ -0,0 +1,16 @@
+let arr = [1,2,3,4,5]
+
+console.log('||||||||||||||SPLICE||||||||||||||||||SPACE||||')
+
+console.log(arr);
+console.log(arr.splice(1, 1)); //remove 1 ELEMENTO do index 1
+console.log(arr);
+
+//spacer
+console.log('||||||||||||||SLICE||||||||||||||||||SPACE||||')
+
+let arr2 = [1,2,3,4,5]
+
+console.log(arr2);
+console.log(arr2.slice(0, 3)); //slice NÃO ALTERA O ARRAY
+console.log(arr2);
